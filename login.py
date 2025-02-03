2e35337b9628e18b9f9a546a063fc58b73fe48fb
kunjangsherpa <kunjangsherpa07@email.com>
2025-02-03 11:13:54 +0545

    checking

--- login.py
+++ login.py
@@ -15,7 +15,7 @@ root.resizable(0,0)
 
 # '''putting background images'''
 
-a=Image.open(r'C:\Users\ACER\Desktop\python\codefreaks\log1.png')
+a=Image.open(r'C:\Users\ASUS\Desktop\Code Freaks\log1.png')
 #resize
 b=a.resize((700,800))
 #need to use this to turn img into tkinter usable format
