fabb1cd262a1d0007b303c9716f5e36fe7795297
suchit <suchitemail.com>
2025-02-20 10:08:18 +0545

    main dasboard test and doctor's dasboard made not integrated

--- dashbaord.py
+++ dashbaord.py
@@ -1,12 +1,14 @@
-from tkinter import*
+from tkinter import *
 from tkinter import messagebox
 from PIL import Image,ImageTk
 root=Tk()
 root.title('Dashboard')
 root.iconbitmap('icon.ico')
 root.geometry('700x500')
-a=Image.open(r'C:\Users\batas\OneDrive\Desktop\Code-Freaks\_DASHBOARD.png')
+a=Image.open(r'_DASHBOARD.png')
 b=a.resize((700,500))
+c=ImageTk.PhotoImage(b)
+lbl=Label(image=c).place(x=0,y=0)
 def Doctor():
     roots=Toplevel()
     roots.title('Doctor page')
@@ -19,8 +21,7 @@ def Patience():
     roots.geometry('700x500')
     roots.iconbitmap('icon.ico')
 
-c=ImageTk.PhotoImage(b)
-lbl=Label(image=c).place(x=0,y=0)
+
 btn=Button(root,text='Doctor',command=Doctor).place(x=120,y=170)
 btn1=Button(text='Patience',command=Patience).place(x=120,y=370)
 root.mainloop()
