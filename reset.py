944f8c4f2fd98f91dfec2d40633f8e8a226a89b2
suchit <suchitemail.com>
2025-02-23 23:49:07 +0545

    database fix

--- reset.py
+++ reset.py
@@ -24,60 +24,48 @@ c=ImageTk.PhotoImage(b)
 l=Label(image=c) 
 l.grid()
 
-def add(event):#If event is not done bring back the place holder.
-    if user.get()=="":
-        user.insert(0,"Enter your Username.")
-        user.config(fg="gray")
-def sub(event):
-    if user.get()=="Enter your Username.":
-        user.delete(0,END)#Clears text
-        user.config(fg="black")
-def addd(event):#If event is not done bring back the place holder.
-    if user.get()=="":
-        user.insert(0,"Enter your old Password.")
-        user.config(fg="gray")
-def subb(event):
-    if user.get()=="Enter your old Password.":
-        user.delete(0,END)#Clears text
-        user.config(fg="black")
-def reset():
-    root.quit()
-    subprocess.Popen(["python","reset.py"])
-def addp(event):#If event is not done bring back the place holder.
-    if pwd.get()=="":
-        pwd.insert(0,"Enter your new Password.")
-        pwd.config(fg="gray")
-def subp(event):#If some event is done remove the place holder
-    if pwd.get()=="Enter your new Password.":
-        pwd.delete(0,END)#Clears text
-        pwd.config(fg="black")
-def addpp(event):#If event is not done bring back the place holder.
-    if pwd.get()=="":
-        pwd.insert(0,"Enter your new Password.")
-        pwd.config(fg="gray")
-def subpp(event):#If some event is done remove the place holder
-    if pwd.get()=="Enter your new Password.":
-        pwd.delete(0,END)#Clears text
-        pwd.config(fg="black")
-def afterlogin():
-    subprocess.Popen(["python","after_login.py"])
-def login():
-    if user.get()=='xyz' and pwd.get()=='xyz':
-        root.quit()
-        afterlogin()
-# '''create a login frame'''
+# def add(event):#If event is not done bring back the place holder.
+#     if user.get()=="":
+#         user.insert(0,"Enter your Username.")
+#         user.config(fg="gray")
+# def sub(event):
+#     if user.get()=="Enter your Username.":
+#         user.delete(0,END)#Clears text
+#         user.config(fg="black")
+# def addd(event):#If event is not done bring back the place holder.
+#     if user.get()=="":
+#         user.insert(0,"Enter your old Password.")
+#         user.config(fg="gray")
+# def subb(event):
+#     if user.get()=="Enter your old Password.":
+#         user.delete(0,END)#Clears text
+#         user.config(fg="black")
+# def reset():
+#     root.quit()
+#     subprocess.Popen(["python","reset.py"])
+# def addp(event):#If event is not done bring back the place holder.
+#     if pwd.get()=="":
+#         pwd.insert(0,"Enter your new Password.")
+#         pwd.config(fg="gray")
+# def subp(event):#If some event is done remove the place holder
+#     if pwd.get()=="Enter your new Password.":
+#         pwd.delete(0,END)#Clears text
+#         pwd.config(fg="black")
+# def addpp(event):#If event is not done bring back the place holder.
+#     if pwd.get()=="":
+#         pwd.insert(0,"Enter your new Password.")
+#         pwd.config(fg="gray")
+# def subpp(event):#If some event is done remove the place holder
+#     if pwd.get()=="Enter your new Password.":
+#         pwd.delete(0,END)#Clears text
+#         pwd.config(fg="black")
+# def afterlogin():
+#     subprocess.Popen(["python","after_login.py"])
+# def login():
+#     if user.get()=='xyz' and pwd.get()=='xyz':
+#         root.quit()
+#         afterlogin()
 
-#authentication
-# def check():
-#     # #database
-    # conn=sqlite3.connect('python.db')
-    # c=conn.cursor()
-    # c.execute(
-    #     """CREATE TABLE IF NOT EXISTS auth(user text,pwd text)"""
-    # )
-    # c.execute('SELECT user FROM auth;')
-    # nam=c.fetchone()
-    # print(nam) #needs work not complete
 #frame
 frame=Frame(root,bd=5)
 #place 75% x and 50% y of display
@@ -86,36 +74,46 @@ frame.place(x=350,y=200)
 col='#000000'
 Login=Label(frame, text="RESET",fg=col,font=('Times new roman',30))
 oldn=Label(frame, text="Old password",fg=col,font=('Times new roman',15))
-old=Entry(frame,font=('Times new roman',15),fg="gray")
-old.insert(0,"Enter your old Password")#initial placeholder
+old=Entry(frame,show='*',font=('Times new roman',15),fg="gray")
+# old.insert(0,"Enter your old Password")#initial placeholder
 name=Label(frame, text="Username",fg=col,font=('Times new roman',15))
 user=Entry(frame,font=('Times new roman',15),fg="gray")
-user.insert(0,"Enter your new Username.")#initial placeholder
-user.bind("<FocusIn>",sub)#focusin is a event and sub is function
-user.bind("<FocusOut>",add)# "   "  "  "
+# user.insert(0,"Enter your new Username.")#initial placeholder
+# user.bind("<FocusIn>",sub)#focusin is a event and sub is function
+# user.bind("<FocusOut>",add)# "   "  "  "
 Password=Label(frame, text="Passsword",fg=col,font=('Times new roman',15))
 pwd=Entry(frame,font=('Times new roman',15),fg="gray")
-pwd.insert(0,"Enter your new Password.")# initial placeholder
-pwd.bind("<FocusIn>",subp)
-pwd.bind("<FocusOut>",addp)
+# pwd.insert(0,"Enter your new Password.")# initial placeholder
+# pwd.bind("<FocusIn>",subp)
+# pwd.bind("<FocusOut>",addp)
 #change password
 def change():
     try:
         user_name=user.get()
-        old_pwd=oldn.get()
+        old_pwd=old.get()
 
         conn=sqlite3.connect('hospital.db')
         cursor=conn.cursor()
         cursor.execute('SELECT user, pwd FROM user' )
         rows=cursor.fetchall()
-        
+        k=0
         for row in rows:
             if row[0]==user_name and row[1]==old_pwd:
                 cursor.execute('UPDATE user SET pwd = ? where user=?',(pwd.get(),user_name))
-            else:
-                messagebox.showerror('Invalid username or password')
-    except:
-        messagebox.showerror('db error')
+                conn.commit()
+                conn.close()
+                k+=1
+        if k==1:
+            messagebox.showinfo('reset done','password has been reset')
+            old.delete(0,END)
+            user.delete(0,END)
+            pwd.delete(0,END)
+        else:
+            messagebox.showerror('Invalid username or password')
+
+    except Exception as e:
+        messagebox.showerror('db error','something went wrong')
+        print(e)
         
 submit=Button(frame,text='CONFIRM',command=change,fg=col,font=('Times new roman',15))
 #display login
